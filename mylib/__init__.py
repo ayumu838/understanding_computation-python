dbd450feb7b1625c4804024eda37009427988216
ayumu838 <ayumu.mochizuki@livesense.co.jp>
2021-05-31 18:15:44 +0900

    add booean, less than

--- mylib/__init__.py
+++ mylib/__init__.py
@@ -2,3 +2,5 @@ from number import Number
 from add import Add
 from multiply import Multiply
 from machine import Machine
+from boolean import Boolean
+from less_than import LessThan
---
+++ mylib/less_than.py
@@ -0,0 +1,21 @@
+from mylib import *
+
+class LessThan:
+  def __init__(self, left, right):
+    self.left = left
+    self.right = right
+  def to_s(self):
+    return '{} < {}'.format(self.left, self.right)
+  def is_reducible(self):
+    return True
+  def reduce(self):
+    if self.left.is_reducible():
+      return LessThan(self.left.reduce(), self.right)
+    elif self.right.is_reducible():
+      return LessThan(self.left, self.right.reduce())
+    else:
+      return Boolean(self.left.value < self.right.value)
+  def __str__(self):
+    return self.to_s()
+  def __repr__(self):
+    return '<<{} < {}>>'.format(self.left, self.right)
